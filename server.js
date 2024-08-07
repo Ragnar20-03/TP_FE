6f8d246983c6701710345b83184209113e157e3d
Ragnar20-03 <roshanpp20@gmail.com>
2024-08-07 20:19:20 +0530

    v2 reconfig

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from "express";
 const app = express();
+
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 dotenv.config();
+
 import { testRouter } from "./routes/testRoute.js";
+// import userRouter from './routes/userRoute.js'
 import registerRouter from "./routes/authRoute.js";
 // import isAuthenticated from "./middleware/isAuth.js";
-import userRouter from './routes/userRoute.js'
 
 app.use(bodyParser.json());
 
@@ -14,20 +16,14 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.json({ message: "Plant the Tree! " });
 });
-// const user = require("./Routes /user");
-// const ngo = require("./Routes /ngo");
-// const volunteer = require("./Routes /volunteer");
 
-// app.use("/user", user);
-// app.use("/ngo", ngo);
-// app.use("/volunteer", volunteer);
 // app.use(isAuthenticated);
 
+// app.use('/user' , userRouter)
 app.use("/auth", registerRouter); // register , update ...  
-
 //!!!TESTING!!!
 app.use("/test", testRouter);
-app.use('/user' , userRouter)
+
 
 app.listen(5100, () => {
   console.log(`Server started at http://localhost:5100`);
