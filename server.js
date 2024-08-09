f371fec9ae96514fd09884baf1a5bb2d7f311f81
Ragnar20-03 <roshanpp20@gmail.com>
2024-08-09 18:55:55 +0530

    Hosted

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import registerRouter from "./routes/authRoute.js";
 import ngoRouter from "./routes/ngoRoute.js";
 import isAuthenticated from "./middleware/isAuth.js";
 
-app.use(isAuthenticated);
+// app.use(isAuthenticated);
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
