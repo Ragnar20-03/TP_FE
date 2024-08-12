e0661ce09c817acc299a2a8175f9aec808bdc226
Ragnar20-03 <roshanpp20@gmail.com>
2024-08-13 02:41:28 +0530

    Login Activity

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -3,35 +3,45 @@ import React, { useState } from "react";
 
 // user9@gmail.com
 // 123456
+
 function Login() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+
+  function onLoginhandler() {
+    console.log("Email is : ", email);
+    console.log("password is : ", password);
+  }
+
   return (
     <div>
-      <div class="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <div class="sm:mx-auto sm:w-full sm:max-w-md">
-          <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+      <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Sign in to your account
           </h2>
-          <p class="mt-2 text-center text-sm text-gray-600 max-w">
+          <p className="mt-2 text-center text-sm text-gray-600 max-w">
             Or
-            <a href="#" class="font-medium text-blue-600 hover:text-blue-500">
+            <a
+              href="#"
+              className="font-medium text-blue-600 hover:text-blue-500"
+            >
               create an account
             </a>
           </p>
         </div>
 
-        <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-          <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form class="space-y-6" action="#" method="POST">
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <form className="space-y-6">
               <div>
                 <label
                   for="email"
-                  class="block text-sm font-medium text-gray-700"
+                  className="block text-sm font-medium text-gray-700"
                 >
                   Email address
                 </label>
-                <div class="mt-1">
+                <div className="mt-1">
                   <input
                     id="email"
                     name="email"
@@ -41,7 +51,7 @@ function Login() {
                     onChange={(e) => {
                       setEmail(e.target.value);
                     }}
-                    class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                     placeholder="Enter your email address"
                   />
                 </div>
@@ -50,11 +60,11 @@ function Login() {
               <div>
                 <label
                   for="password"
-                  class="block text-sm font-medium text-gray-700"
+                  className="block text-sm font-medium text-gray-700"
                 >
                   Password
                 </label>
-                <div class="mt-1">
+                <div className="mt-1">
                   <input
                     onChange={(e) => {
                       setPassword(e.target.value);
@@ -64,32 +74,32 @@ function Login() {
                     type="password"
                     autocomplete="current-password"
                     required
-                    class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                     placeholder="Enter your password"
                   />
                 </div>
               </div>
 
-              <div class="flex items-center justify-between">
-                <div class="flex items-center">
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
                   <input
                     id="remember_me"
                     name="remember_me"
                     type="checkbox"
-                    class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
                   />
                   <label
                     for="remember_me"
-                    class="ml-2 block text-sm text-gray-900"
+                    className="ml-2 block text-sm text-gray-900"
                   >
                     Remember me
                   </label>
                 </div>
 
-                <div class="text-sm">
+                <div className="text-sm">
                   <a
                     href="#"
-                    class="font-medium text-black hover:text-gray-500"
+                    className="font-medium text-black hover:text-gray-500"
                   >
                     Forgot your password?
                   </a>
@@ -99,36 +109,35 @@ function Login() {
               <div>
                 <button
                   onClick={() => {
-                    console.log("email is : ", email);
-                    console.log("password is : ", password);
+                    onLoginhandler();
                   }}
-                  type="btn"
-                  class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-teal-700 hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  type="button"
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-teal-700 hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   Sign in
                 </button>
               </div>
             </form>
-            <div class="mt-6">
-              <div class="relative">
-                <div class="absolute inset-0 flex items-center">
-                  <div class="w-full border-t border-gray-300"></div>
+            <div className="mt-6">
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <div className="w-full border-t border-gray-300"></div>
                 </div>
-                <div class="relative flex justify-center text-sm">
-                  <span class="px-2 bg-gray-100 text-gray-500">
+                <div className="relative flex justify-center text-sm">
+                  <span className="px-2 bg-gray-100 text-gray-500">
                     Or continue with
                   </span>
                 </div>
               </div>
 
-              <div class="mt-6 grid grid-cols-3 gap-3">
+              <div className="mt-6 grid grid-cols-3 gap-3">
                 <div>
                   <a
                     href="#"
-                    class="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
                   >
                     <img
-                      class="h-5 w-5"
+                      className="h-5 w-5"
                       src="https://www.svgrepo.com/show/512120/facebook-176.svg"
                       alt=""
                     />
@@ -137,10 +146,10 @@ function Login() {
                 <div>
                   <a
                     href="#"
-                    class="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
                   >
                     <img
-                      class="h-5 w-5"
+                      className="h-5 w-5"
                       src="https://www.svgrepo.com/show/513008/twitter-154.svg"
                       alt=""
                     />
@@ -149,10 +158,10 @@ function Login() {
                 <div>
                   <a
                     href="#"
-                    class="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
                   >
                     <img
-                      class="h-6 w-6"
+                      className="h-6 w-6"
                       src="https://www.svgrepo.com/show/506498/google.svg"
                       alt=""
                     />
@@ -163,8 +172,8 @@ function Login() {
           </div>
         </div>
       </div>
-      <footer className="bg-teal-700 text-white py-6 mt-auto">
-        <div className="container mx-auto text-center text-sm">
+      <footer classNameName="bg-teal-700 text-white py-6 mt-auto">
+        <div classNameName="container mx-auto text-center text-sm">
           &copy; 2024 Tree Planter. All rights reserved.
         </div>
       </footer>
